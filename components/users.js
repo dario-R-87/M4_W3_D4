e6a56c3607079f32d58d5bee546a42e4d22bf05e
Dario Russo <dariorusso987@gmail.com>
2024-01-20 12:05:25 +0100

    update

--- components/users.js
+++ components/users.js
@@ -1,55 +1,63 @@
 import { getTableRows } from "./table.js";
+import { getCurrentInputValue } from "../index.js";
 
 let users = [];
 let tableBody = "";
 const table = document.querySelector("#table_body");
-
 export const getUsers = async (url) => {
-    try {
-        const response = await fetch(url);
-        users = await response.json();
-        tableBody = getTableRows(users);
-        table.innerHTML = tableBody;
-        const filter = localStorage.getItem("filterBy") ? localStorage.getItem("filterBy") : "select";
-        const value = localStorage.getItem("filterValue") ? localStorage.getItem("filterValue") : "";
-        if(value!=="" && filter!=="select"){
-            filterUsers();
-        }
-        toggleLoader(false);
-    } catch (e) { console.error(e); }
-}
+  try {
+    const response = await fetch(url);
+    users = await response.json();
+    tableBody = getTableRows(users);
+    table.innerHTML = tableBody;
+    const values = getCurrentInputValue();
+    const filter = values.filterType;
+    const value = values.filterValue;
+    if (value !== "" && filter !== "select") {
+      filterUsers();
+    }
+    toggleLoader(false);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 export const filterUsers = () => {
+  const values = getCurrentInputValue();
+  console.log(values);
+  const filter = values.filterType;
+  const value = values.filterValue;
+  console.log(filter);
+  console.log(value);
 
-    const filter = localStorage.getItem("filterBy") ? localStorage.getItem("filterBy") : "select";
-    const value = localStorage.getItem("filterValue") ? localStorage.getItem("filterValue") : "";
-
-    if(!filter || filter === "select"){
-        const select = document.querySelector("#select");
-        select.classList.add("error"); 
-        alert("Please, select a valid filter type!");
-    } else if (value===""){
-        const input = document.querySelector("#search");
-        input.classList.add("error"); 
-        alert("Please, insert text");
-    } else {
-        toggleLoader(true);
-        const filteredUsers = users.filter(
-            (user) => user[filter].toLowerCase().indexOf(value.toLowerCase().trim()) !== -1);
-        tableBody = getTableRows(filteredUsers);
-        table.innerHTML = tableBody;
-        toggleLoader(false);
-    }
+  if (!filter || filter === "select") {
+    const select = document.querySelector("#select");
+    select.classList.add("error");
+    alert("Please, select a valid filter type!");
+  } else if (value === "") {
+    const input = document.querySelector("#search");
+    input.classList.add("error");
+    alert("Please, insert text");
+  } else {
+    toggleLoader(true);
+    const filteredUsers = users.filter(
+      (user) =>
+        user[filter].toLowerCase().indexOf(value.toLowerCase().trim()) !== -1
+    );
+    tableBody = getTableRows(filteredUsers);
+    table.innerHTML = tableBody;
+    toggleLoader(false);
+  }
 };
 
 export const toggleLoader = (flag) => {
-    if(flag){
-        document.querySelector(".loader").classList.remove("d-none");
-        document.querySelector(".table").classList.add("d-none");
-    } else {
-        setTimeout(()=>{
-            document.querySelector(".loader").classList.add("d-none");
-            document.querySelector(".table").classList.remove("d-none");
-        },1500);
-    }
+  if (flag) {
+    document.querySelector(".loader").classList.remove("d-none");
+    document.querySelector(".table").classList.add("d-none");
+  } else {
+    setTimeout(() => {
+      document.querySelector(".loader").classList.add("d-none");
+      document.querySelector(".table").classList.remove("d-none");
+    }, 1500);
+  }
 };
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import {getUsers, filterUsers, toggleLoader} from "./components/users.js";
+import { getUsers, filterUsers, toggleLoader } from "./components/users.js";
 
-export const getCurrentInputValue = ()=>{
-    const values = {
-        filterType: localStorage.getItem("filterBy") ? localStorage.getItem("filterBy") : "select",
-        filterValue: localStorage.getItem("filterValue") ? localStorage.getItem("filterValue") : "",
-    }
-    return values;
- }
+export const getCurrentInputValue = () => {
+  const values = {
+    filterType: localStorage.getItem("filterBy")
+      ? localStorage.getItem("filterBy")
+      : "select",
+    filterValue: localStorage.getItem("filterValue")
+      ? localStorage.getItem("filterValue")
+      : "",
+  };
+  return values;
+};
 
-const url ="https://jsonplaceholder.typicode.com/users";
+const url = "https://jsonplaceholder.typicode.com/users";
 const select = document.querySelector("#select");
 const input = document.querySelector("#search");
 const values = getCurrentInputValue();
@@ -18,39 +22,34 @@ const submitBtn = document.querySelector("#submit");
 submitBtn.addEventListener("click", filterUsers);
 const deleteBtn = document.querySelector("#empty");
 
-
-
-
 getUsers(url);
 
 const storeFilterSelected = (event) => {
-    if(event.target.value!=="select")
-        select.classList.remove("error"); 
-    localStorage.setItem("filterBy",event.target.value);
-}
+  if (event.target.value !== "select") select.classList.remove("error");
+  localStorage.setItem("filterBy", event.target.value);
+};
 
 const storeInputValue = (event) => {
-    if(event.target.value!=="select")
-         input.classList.remove("error"); 
-    localStorage.setItem("filterValue",event.target.value);
- }
+  if (event.target.value !== "select") input.classList.remove("error");
+  localStorage.setItem("filterValue", event.target.value);
+};
 
- const deleteFilter = ()=>{
-    toggleLoader(true);
-    getUsers(url);
-    input.value="";
-    select.value="select"
-    localStorage.setItem("filterBy","select");
-    localStorage.setItem("filterValue","");
-    toggleLoader(false);
- };
+const deleteFilter = () => {
+  toggleLoader(true);
+  getUsers(url);
+  input.value = "";
+  select.value = "select";
+  localStorage.setItem("filterBy", "select");
+  localStorage.setItem("filterValue", "");
+  toggleLoader(false);
+};
 
-input.addEventListener("keyup", (e)=>{
-    storeInputValue(e);
+input.addEventListener("keyup", (e) => {
+  storeInputValue(e);
 });
 
-select.addEventListener("change", (e)=>{
-    storeFilterSelected(e);
+select.addEventListener("change", (e) => {
+  storeFilterSelected(e);
 });
 
 deleteBtn.addEventListener("click", deleteFilter);
